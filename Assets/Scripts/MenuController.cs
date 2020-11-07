fc8bd2a8df9e2c87297fa34365ff5194d9885e3f
KasparasKralikas <kasparaskr@unity3d.com>
2020-11-07 17:10:41 +0200

    Added initial MenuController

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public static string firstSceneName = "Level1";
+
+    public static void Continue()
+    {
+        // TODO: add progress tracking
+        throw new NotImplementedException();
+    }
+
+    public static void NewGame()
+    {
+        SceneManager.LoadScene(firstSceneName);
+    }
+
+    public static void Options()
+    {
+        // TODO: add Options panel
+        throw new NotImplementedException();
+    }
+
+    public static void Exit()
+    {
+        Application.Quit();
+    }
+}
