4bbd399993cad49f751561bf59f8fb42747f25ac
KernalPanik <lukas.michnevic@protonmail.com>
2020-11-14 17:40:05 +0200

    Added secret and level end trigger

---
+++ Assets/Scripts/Level/ArenaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject arenaExitDoor;
+
+    [SerializeField]
+    private List<GameObject> enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arenaExitDoor.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemies.Count == 0)
+        {
+            arenaExitDoor.SetActive(true);
+        }   
+    }
+}
---
+++ Assets/Scripts/Level/LevelEndTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("The level is finished!");
+        }
+    }
+}
