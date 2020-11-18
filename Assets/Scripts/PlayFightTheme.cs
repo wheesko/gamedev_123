8e9a74469934b1582ae33c9b50fd61d66f3c55af
wheesko <vytautsrudys@gmail.com>
2020-11-18 21:35:35 +0200

    Added music to demo level and main menu

---
+++ Assets/Scripts/PlayFightTheme.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFightTheme : MonoBehaviour
+{
+    public GameObject entryPoint;
+
+    public MusicController musicController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered area to play music");
+            musicController.GetComponent<MusicController>().Play(MusicController.MusicType.FIGHT);
+        }
+    }
+}
