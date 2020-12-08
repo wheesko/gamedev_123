b5417775e01b2fe2a10b97933453a02c2d2dd544
KernalPanik <lukas.michnevic@protonmail.com>
2020-12-08 18:52:25 +0200

    saving status

---
+++ Assets/Scripts/ArenaTriggerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaTriggerController : MonoBehaviour
+{
+    public GameObject ArenaTrapDoor;
+    private bool arenaTriggered = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !arenaTriggered)
+        {
+            ArenaTrapDoor.SetActive(true);
+            arenaTriggered = true;
+        }
+    }
+}
