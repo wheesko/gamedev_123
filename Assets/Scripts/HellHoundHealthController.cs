9fab0dcd74567a34d90960d1d30f90b64486ea51
KernalPanik <lukas.michnevic@protonmail.com>
2020-12-09 22:01:27 +0200

    Finalized level 1

---
+++ Assets/Scripts/HellHoundHealthController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HellHoundHealthController : EnemyHealthController
+{
+    public Animator animator;
+    public void Update()
+    {
+        if(currentHealth <= 0)
+        {
+            animator.SetTrigger("Death");
+            GetComponent<EnemyController>().enabled = false;
+            GetComponent<NavMeshAgent>().enabled = false;
+            Destroy(gameObject, 3.533f);
+        }
+    }
+}
