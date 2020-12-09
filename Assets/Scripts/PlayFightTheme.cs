1166e015de45ebff2f070de3c9dbde90ce4a8db7
KernalPanik <lukas.michnevic@protonmail.com>
2020-12-09 22:30:56 +0200

    finalized level for sure

--- Assets/Scripts/PlayFightTheme.cs
+++ Assets/Scripts/PlayFightTheme.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayFightTheme : MonoBehaviour
 {
     public GameObject entryPoint;
+    private bool musicStarted = false;
 
     public MusicController musicController;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !musicStarted)
         {
+            musicStarted = true;
             Debug.Log("Player entered area to play music");
             musicController.GetComponent<MusicController>().Play(MusicController.MusicType.Fight);
         }
